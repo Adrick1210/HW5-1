9f05de5d017b72689935c2c9d131fc814e256c69
Christian Alexander <christian.alexanderato@gmail.com>
2023-11-30 17:58:46 -0800

    commit 1 - added my first element and it was good

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -1,3 +1,13 @@
-
-  
-  
+$(() => {
+    // create the container variable
+    $container = $('#container');
+    // console.log($container)
+    // create the h1 variable that creates a h1 tag
+    $h1 = $('<h1>')
+    // console.log($h1);
+    // add inner text to the html
+    $h1.text('Hogwarts')
+    // console.log($h1)
+    // append the h1 to the div
+    $container.append($h1);
+});
