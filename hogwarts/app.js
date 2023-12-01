902bb52622c3c298a685e78bd193f8b7e0d4c02a
Christian Alexander <christian.alexanderato@gmail.com>
2023-11-30 18:08:25 -0800

    commit 2 - I added more elements. My power grows

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -1,4 +1,5 @@
 $(() => {
+    // Year One
     // create the container variable
     $container = $('#container');
     // console.log($container)
@@ -10,4 +11,15 @@ $(() => {
     // console.log($h1)
     // append the h1 to the div
     $container.append($h1);
+    
+    // Year Two
+    // Add h2 of name
+    $h2 = $('<h2>').text('Adrick Ravenwood');
+    $container.append($h2);
+    // Add h3 for house
+    $h3 = $('<h3>').text('Gryffindor');
+    $container.append($h3);
+    // add h4 for pet
+    $h4 = $('<h4>').addClass('cat').text('Squeaks');
+    $container.append($h4);
 });
